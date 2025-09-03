ffd9dc1b04110b7c7b39fa764f6bc21606e8eaca
katuhhr <kataharina07@gmail.com>
2025-09-03 15:10:44 +0300

    1

---
+++ мдк05.02/DKR/задача 1.pas
@@ -0,0 +1,22 @@
+﻿program dkr;
+var x,y: real;
+
+begin
+    writeln('введите х: ');  
+    readln(x);
+    if (x <= (-7)) then 
+      begin
+        y := cos(2*x)/sin(x)+ln(abs(x));
+        writeln('x=',x,', y=',y);
+      end;
+    if (x <= -4) and (x >= -7) then 
+      begin
+        y:=sin(x) / cos(2*x); 
+        writeln('x=',x,', y=',y);
+      end;
+    if x>= -4 then
+      begin
+        y:=(tan(x) * (x * x));
+        writeln('x=',x,', y=',y);
+      end;     
+end.
---
+++ мдк05.02/DKR/задача 2.pas
@@ -0,0 +1,25 @@
+﻿program dkr;
+var x,y,l: real;
+begin
+  x := -9;
+  l := 0.1;
+  while x <= -2 do
+  begin
+    if (x <= (-7)) then 
+      begin
+        y := cos(2*x)/sin(x)+ln(abs(x));
+        writeln('x=',x:2:1,', y=',y:2:3);
+      end;
+    if (x <= -4) and (x >= -7) then 
+      begin
+        y:=sin(x) / cos(2*x); 
+        writeln('x=',x:2:1,', y=',y:2:3);
+      end;
+    if x>= -4 then
+      begin
+        y:=(tan(x) * (x * x));
+        writeln('x=',x:2:1,', y=',y:2:3);
+      end;
+       x:=x+l;
+end;
+end.
